b76a360a4f0b76596527ad4510c63553092e641a
enotdima <20352302+enotdima@users.noreply.github.com>
2021-01-30 22:54:38 +0300

    Revert unneded

--- examples/wgan_example.py
+++ examples/wgan_example.py
@@ -9,8 +9,8 @@ import numpy as np
 import sklearn.cluster as cluster
 import matplotlib.pyplot as plt
 
-from ydata_synthetic.synthesizers.regular import WGAN_GP
-from ydata_synthetic.preprocessing.regular.credit_fraud import transformations
+from ydata_synthetic.preprocessing.adult import transformations
+from ydata_synthetic.synthesizers import WGAN_GP
 
 model = WGAN_GP
 
@@ -83,7 +83,6 @@ synth = WGAN_GP.load(path='models/wgan_creditcard.pkl')
 #Sampling the data
 #Note that the data returned it is not inverse processed.
 data_sample = synth.sample(100000)
-print(data_sample)
 print('Testing the sample of data.')
 
 #Sample events using the trained model
